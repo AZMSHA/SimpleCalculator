43fefcc40e40431a2d9c8bfe95de78f4f40e3af2
Hashir Adnan <azmsha2003@gmail.com>
2023-08-05 07:41:38 -0700

    app functionality done, css remaining

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const [result, setResult] = useState(0);
+  function operation(operator) {
+    const input = inputRef.current.value;
+    switch (operator) {
+      case "+":
+        setResult(result + input);
+        break;
+
+      case "-":
+        setResult(result - input);
+        break;
+
+      case "*":
+        setResult(result * input);
+        break;
+
+      case "/":
+        setResult(result / input);
+        break;
+
+      case "reset input":
+        inputRef.current.value = 0;
+        break;
+
+      case "reset result":
+        setResult(0);
+        break;
+
+      default:
+        break;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h1>{result}</h1>
+      <input type="text" ref={inputRef} />
+      <Button operator={"+"}>ADD</Button>
+      <Button operator={"-"}>SUBTRACT</Button>
+      <Button operator={"*"}>MULTIPLY</Button>
+      <Button operator={"/"}>DIVIDE</Button>
+      <Button operator={"reset input"}>RESET INPUT</Button>
+      <Button operator={"reset result"}>RESET RESULT</Button>
+    </form>
   );
+
+  function Button({operator,children}) {
+    return <button onClick={() => operation(operator)}>{children}</button>;
+  }
 }
 
 export default App;
